a00f6179cf97d5cdf33c77f818dee2dd2c3dfbb6
Owen van Dijk <owen.van.dijk@gmail.com>
2014-03-07 18:01:57 +0100

    Added build files to ignore

--- CinderGrid/src/CinderGridApp.cpp
+++ CinderGrid/src/CinderGridApp.cpp
@@ -1,5 +1,6 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "cinder/Perlin.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -11,6 +12,7 @@ class CinderGridApp : public AppNative {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+    int frameCount = 0;
 };
 
 void CinderGridApp::setup()
@@ -27,8 +29,20 @@ void CinderGridApp::update()
 
 void CinderGridApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+    
+    Perlin perlin;
+    for(int y = 0; y < 100; y++) {
+        for(int x = 0; x < 100; x++) {
+            float noiseVal = perlin.noise(x * 0.05, y * 0.15, frameCount / 50.0f);
+            float r = ((0.3 + noiseVal) * (0.3 + noiseVal)) * 8.;
+            float gridSpace = 10;
+            gl::color(noiseVal + 0.3, 1.0 - (noiseVal * 0.5), (noiseVal + 0.8) / 2.);
+            gl::drawSolidCircle( Vec2f((gridSpace * x), (gridSpace * y)) , r);
+        }
+    }
+    
+    frameCount++;
 }
 
 CINDER_APP_NATIVE( CinderGridApp, RendererGl )
